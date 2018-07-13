ad228e08f67d7c46554cdbb8bdff2d1469272eec
ailtonbsj <ailton.ifce@gmail.com>
2018-07-13 15:00:54 -0300

    fixed bug

--- schedule.js
+++ schedule.js
@@ -2,10 +2,6 @@ var Orkidea = (function(){
 
 var palette = ['#001f3f','#0074D9','#7FDBFF','#39CCCC','#3D9970','#2ECC40',
 '#01FF70','#FFDC00','#FF851B','#FF4136','#85144b','#F012BE','#B10DC9'];
-var inxdexColor = 0;
-var listDates = [];
-var listYLabels = [];
-var listXLabels = [];
 var contYAxis = 0;
 var allPad = 1;
 var majorLabelWidth = 0;
@@ -14,22 +10,27 @@ var jumph = 1;
 var configs = {};
 
 function Schedule(domId, data, config){
+	var self = this;
+	this.indexColor = 0;
+	self.listDates = [];
+	self.listYLabels = [];
+	self.listXLabels = [];
 	configs = config;
 	data.map(function(item){
 		var initDate = item.inittime.split(' ')[0];
 		var finalDate = item.finaltime.split(' ')[0];
 		if(initDate != finalDate)
 			console.warn('This component does not suport interval major than one day!');
-		if(listDates[initDate] == undefined){
-			listDates[initDate] = [];
-			listDates[initDate].push(item);
-		} else listDates[initDate].push(item);
+		if(self.listDates[initDate] == undefined){
+			self.listDates[initDate] = [];
+			self.listDates[initDate].push(item);
+		} else self.listDates[initDate].push(item);
 	});
 
 	var root = document.getElementById(domId);
 	root.innerHTML = '';
 
-	for(date in listDates){
+	for(date in self.listDates){
 
 		var wrap = document.createElement("div");
 		root.appendChild(wrap);
@@ -39,20 +40,20 @@ function Schedule(domId, data, config){
 		wrap.appendChild(panel);
 		panel.classList.add("orkidea-schedule-panel");
 		panel.innerHTML = '';
-		listYLabels = [];
-		listXLabels = [];
+		self.listYLabels = [];
+		self.listXLabels = [];
 		contYAxis = 0;
 		majorLabelWidth = 0;
-		createYLabel(panel, date);
+		self.createYLabel(panel, date);
 
-		listDates[date].map(function(item){
-			if(listYLabels[item.yAxis] == undefined)
-				createYLabel(panel, item.yAxis);
+		self.listDates[date].map(function(item){
+			if(self.listYLabels[item.yAxis] == undefined)
+				self.createYLabel(panel, item.yAxis);
 
 		});
 
-		for(label in listYLabels){
-			listYLabels[label].style.width = majorLabelWidth + 'px';
+		for(label in self.listYLabels){
+			self.listYLabels[label].style.width = majorLabelWidth + 'px';
 		}
 
 		widthPanel = panel.clientWidth - majorLabelWidth;
@@ -72,39 +73,46 @@ function Schedule(domId, data, config){
 		}
 
 		for(var i=0; i<24; i+=jumph){
-			createXLabel(panel, i+'h', majorLabelWidth + i*hourDist);
+			self.createXLabel(panel, i+'h', majorLabelWidth + i*hourDist);
 		}
 
-		listDates[date].map(function(item){
+		self.listDates[date].map(function(item){
 			var time = item.inittime.split(' ')[1];
 			var tk = time.split(':');
 			var secondInit = (+tk[0]) * 60 * 60 + (+tk[1]) * 60 + (+tk[2]);
 			time = item.finaltime.split(' ')[1];
 			tk = time.split(':');
 			var secondFinal = (+tk[0]) * 60 * 60 + (+tk[1]) * 60 + (+tk[2]);
-			var _y = listYLabels[item.yAxis].offsetTop;
+			var _y = self.listYLabels[item.yAxis].offsetTop;
 			var _x = majorLabelWidth + widthPanel*(secondInit/86399); //linear interpolation
 			var _w = majorLabelWidth + widthPanel*(secondFinal/86399);
 			_w = _w - _x;
-			createItem(item.text+"\nStart: "+item.inittime+"\nEnd: "+item.finaltime,_x, _y, _w, item.color, panel);
+			self.createItem(item.text+"\nStart: "+item.inittime+"\nEnd: "+item.finaltime,_x, _y, _w, item.color, panel);
 		});
 	}
 }
-
-function createItem(value, x, y, w, bgColor, parent){
+$Schedule = Schedule.prototype;
+//Properties
+$Schedule.indexColor = 0;
+$Schedule.listDates = [];
+$Schedule.listYLabels = [];
+$Schedule.listXLabels = [];
+//Methods
+$Schedule.createItem = function(value, x, y, w, bgColor, parent){
 	var node = document.createElement("div"	);
 	node.style.left = x+'px';
 	node.style.top = y+allPad+'px';
 	node.style.width = w+'px';
 	node.style.height = lenY+'px';
 	if(configs.uniqueColor == undefined){
-		node.style.background = palette[inxdexColor];
-		inxdexColor = inxdexColor < 12 ? inxdexColor+1 : 0;
+		node.style.background = palette[this.indexColor];
+		this.indexColor = this.indexColor < 12 ? this.indexColor+1 : 0;
 	} 
 	else if(configs.uniqueColor == 'indata'){
 		if(bgColor == undefined){
-			node.style.background = palette[inxdexColor];
-			inxdexColor = inxdexColor < 12 ? inxdexColor+1 : 0;
+			//console.log(this.indexColor);
+			node.style.background = palette[this.indexColor];
+			this.indexColor = this.indexColor < 12 ? this.indexColor+1 : 0;
 		} else node.style.background = bgColor;
 	} else node.style.background = configs.uniqueColor;
 	node.style.overflow = 'hidden';
@@ -115,18 +123,16 @@ function createItem(value, x, y, w, bgColor, parent){
 		alert(this.getAttribute('title'));
 	};
 }
-
-function createXLabel(parent, value, left){
+$Schedule.createXLabel = function(parent, value, left){
 	var node = document.createElement("div"	);
 	node.innerHTML = value;
 	node.style.left = left+'px';
 	node.style.width = hourDist + 'px';
 	parent.appendChild(node);
 	node.style.top = '0';
-	listXLabels.push(node);
+	this.listXLabels.push(node);
 }
-
-function createYLabel(parent, value){
+$Schedule.createYLabel = function(parent, value){
 	var node = document.createElement("div");
 	node.innerHTML = value;
 	parent.appendChild(node);
@@ -135,7 +141,7 @@ function createYLabel(parent, value){
 	if(majorLabelWidth < node.clientWidth+allPad){
 		majorLabelWidth = node.clientWidth+allPad;
 	}
-	listYLabels[value] = node;
+	this.listYLabels[value] = node;
 	node.classList.add("orkidea-schedule-labelY");
 	var white = document.createElement("DIV");
 	white.innerHTML = "&nbsp";
